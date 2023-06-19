35ea57bd5735956ddbfa3786670c2cc07b24fa80
LssLn <lssln97@gmail.com>
2023-06-19 21:10:52 +0200

    init

---
+++ src/test/java/redbite/io/kafkamanager/KafkaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package redbite.io.kafkamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
